ca1c93684dc940d1d5540fc2e8fea2f21505edb8
Daniel Drion <daniel.drion@telenet.be>
2024-12-10 17:01:38 +0100

    Added nullable anotation in DiaryEntry model

--- WebDiaryAPI/Models/DiaryEntry.cs
+++ WebDiaryAPI/Models/DiaryEntry.cs
@@ -6,9 +6,9 @@ namespace WebDiaryAPI.Models
     {
         public int Id { get; set; }
         [Required]
-        public string Title { get; set; }
+        public string? Title { get; set; }
         [Required]
-        public string Content { get; set; }
+        public string? Content { get; set; }
         [Required]
         public DateTime Created { get; set; } = DateTime.Now;
     }
